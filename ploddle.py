955a348afd9b797b648c50a53b2a7943e2daa309
Shish <shish@shishnet.org>
2012-09-23 12:45:14 +0100

    only use the filename as daemon name, not the whole path

--- ploddle.py
+++ ploddle.py
@@ -4,6 +4,7 @@ from logging.handlers import SysLogHandler
 import logging
 import sys
 import json
+import os
 
 
 class PloddleFormatter(logging.Formatter):
@@ -45,7 +46,7 @@ class PloddleFormatter(logging.Formatter):
 class PloddleHandler(SysLogHandler):
     def __init__(self, addr, port, daemonName=None):
         if not daemonName:
-            daemonName = sys.argv[0]
+            daemonName = os.path.basename(sys.argv[0])
         logging.handlers.SysLogHandler.__init__(self, address=(addr, port))
         self.setFormatter(PloddleFormatter(daemonName))
 
