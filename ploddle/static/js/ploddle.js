061a7d11282c139ed8ff5325ae262a954df90997
Shish <shish@shishnet.org>
2012-09-30 00:53:51 +0100

    remove old bits of ploddle.js

--- ploddle/static/js/ploddle.js
+++ ploddle/static/js/ploddle.js
@@ -1,184 +1,5 @@
-var columns = {
-	"severity": {
-		name: "Sev.",
-		filter: '<select name="severity" id="severity">'+
-			'<option value="2">Crit</option>'+
-			'<option value="3">Error</option>'+
-			'<option value="4">Warn</option>'+
-			'<option value="6">Info</option>'+
-			'<option value="7" selected>Debug</option>'+
-		'</select>',
-		init: function() {
-			$("#severity").change(get_data);
-		},
-		render: function(row) {
-			return row["severity"];
-		},
-	},
-	"host": {
-		name: "Host",
-		filter: '<select name="host" id="host"></select>',
-		init: function() {
-			json_to_select("/api/hosts.json", "#host");
-		},
-	},
-	"timestamp": {
-		name: "Timestamp",
-		filter: '<input name="timestamp" id="timestamp" type="date">',
-		render: function(row) {
-			return row["timestamp"].substring(0, 16);
-		},
-	},
-	"daemon": {
-		name: "Daemon",
-		filter: '<select name="daemon" id="daemon"></select>',
-		init: function() {
-			json_to_select("/api/daemons.json", "#daemon");
-		},
-	},
-	"threadName": {
-		name: "Thread",
-		filter: '<input name="threadName" id="threadName">',
-	},
-	"name": {
-		name: "Logger Name",
-		filter: '<input name="name" id="name">',
-	},
-	"source1": {
-		name: "Source Function",
-		filter: '<input name="module" id="module">',
-		render: function(row) {
-			if(row["module"]) {
-				return row["module"]+":"+row["funcName"];
-			}
-			else {
-				return "-";
-			}
-		},
-	},
-	"source2": {
-		name: "Source Code",
-		filter: '<input name="module" id="module">',
-		render: function(row) {
-			if(row["filename"]) {
-				return row["filename"]+":"+row["lineno"];
-			}
-			else {
-				return "-";
-			}
-		},
-	},
-	"message": {
-		name: "Message",
-		filter: '<input name="message" id="message">',
-	},
-	"debug": {
-		name: "Debug Info",
-		filter: '',
-		render: function(row) {
-			return JSON.stringify(row);
-		},
-	},
-};
 
-var active_columns = [
-	"host",
-	"timestamp",
-	"daemon",
-	"message",
-];
 
-function json_to_select(url, select) {
-	$.getJSON(url, {}, function(data) {
-    	$(select).empty();
-		$(select).append($('<option value="">All</option>'));
-		$(data).each(function(i, val) {
-			$(select).append($('<option value="'+val+'">'+val+'</option>'));
-    	});
-	});
-}
-
-function render_header() {
-	var titles = $('<tr/>');
-	var filters = $("<tr/>");
-	$(active_columns).each(function(f, colname) {
-		titles.append($("<td/>").text(columns[colname].name));
-		filters.append($("<td/>").html(columns[colname].filter));
-	});
-
-	$("#headings").empty();
-	$("#headings").append(titles);
-	$("#headings").append(filters);
-
-	$(active_columns).each(function(f, colname) {
-		if(columns[colname].init) {
-			columns[colname].init();
-		}
-	});
-
-	$("#headings INPUT").change(get_data_event);
-	$("#headings SELECT").change(get_data_event);
-
-	$(".main").css("top", $("HEADER").height()+1);
-	$(".main").css("bottom", $("FOOTER").height()+1);
-}
-
-function render_colsel() {
-	var list = $("<ul/>");
-	$.each(columns, function(colname, colspec) {
-		checked = $.inArray(colname, active_columns) >= 0 ? " checked" : "";
-		list.append($("<li><label><input type='checkbox' name='"+colname+"' value='"+colname+"'"+checked+"> "+columns[colname].name+"</label>"));
-	});
-	$("#columns").append(list);
-
-	$("#columns input[type='checkbox']").click(function(e) {
-		console.log("Updating columns");
-		active_columns = [];
-		$("#columns input[type='checkbox']").each(function(i, el) {
-			if($(el).is(":checked")) {
-				active_columns.push($(el).val());
-			}
-		});
-		render_header();
-		get_data();
-	});
-}
-
-function is_live() {
-	return $("#livemode").is(":checked");
-}
-
-function render_paginator(p, ps) {
-	$("#page").text(p);
-	$("#pages").text(ps);
-}
-
-function render_row(row) {
-	var html_row = $("<tr class='message' />");
-	if(row["severity"]) {
-		html_row.addClass("severity-"+row["severity"]);
-	}
-	$(active_columns).each(function(f, colname) {
-		var renderer = columns[colname].render ? columns[colname].render : function(row) {return row[colname]};
-		var rowel = $("<td/>");
-		rowel.text(renderer(row));
-		rowel.addClass(colname);
-		html_row.append(rowel)
-	});
-    $("#messages").append(html_row);
-}
-
-function fix_header_widths() {
-	$("#headings TR:eq(1) TD").each(function(i, el) {
-		$(el).width(
-			$("#messages TR:eq(1) TD:eq("+i+")").width()
-		);
-	});
-}
-
-function get_data_event(e) {
-	get_data();
-}
 
 function get_data(append) {
 	if(!append) {
@@ -210,15 +31,6 @@ function get_data(append) {
 	});
 }
 
-function go_to_page(page) {
-	if(page >= 1 && page <= parseInt($("#pagesinput").val())) {
-		$("#pageinput").val(page);
-		get_data();
-	}
-}
-function go_to_relpage(relpage) {
-	go_to_page(parseInt($("#pageinput").val()) + relpage);
-}
 
 var live_timer = false;
 function start_live() {
@@ -240,28 +52,6 @@ function cancel_live() {
 }
 
 $(function() {
-	render_colsel();
-	render_header();
-	render_paginator(1, 1);
-	get_data();
-
-	$("#firstpage").click(function(e) {
-		go_to_page(1);
-		return false;
-	});
-	$("#prevpage").click(function(e) {
-		go_to_relpage(-1);
-		return false;
-	});
-	$("#nextpage").click(function(e) {
-		go_to_relpage(1);
-		return false;
-	});
-	$("#lastpage").click(function(e) {
-		go_to_page(parseInt($("#pagesinput").val()));
-		return false;
-	});
-
 	$("#livemode").click(function(e) {
 		if(is_live()) {
 			start_live();
@@ -269,10 +59,5 @@ $(function() {
 		else {
 			cancel_live();
 		}
-	});
-
-	$("#showsettings").click(function(e) {
-		$(".settings").toggle();
-		return false;
 	});
 });
